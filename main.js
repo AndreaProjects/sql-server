7da8b52a0a2d2329e46c0da7465d4b0140f2420c
andreaemmi <andreaemmidev@gmail.com>
2024-05-24 17:06:41 +0200

    added express

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+const express = require("express");
 var mysql = require("mysql");
+const app = express();
+app.use(express.json());
 
 var con = mysql.createConnection({
   host: "192.34.62.4", // Host del database
-  user: "cE4yGn3dCjua", // Userna1me del database
+  user: "andrea", // Userna1me del database
   password: "A5twXYw0rWbflCv7", // Password del database
   database: "masstime-wp-TxkNuiUZ",
   debug: true,
 });
 
-con.connect(function (err) {
-  console.log("Connected! ");
-  console.log(err);
+app.get("/", (req, res) => {
+  con.connect(function (err) {
+    console.log("Connected! ");
+    console.log(err);
+  });
 });
